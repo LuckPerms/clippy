0420df75ea65ed7774f6188d7330e8d1d58b442c
Jay Williams <course@jaywilliams.me>
2018-12-15 19:45:51 +0000

    Revert new ping system

--- modules/no_ping.js
+++ modules/no_ping.js
@@ -1,18 +1,6 @@
 const data = require("../data.json")
 const fs = require("mz/fs");
 
-async function no_ping_regular_handler(msg) {
-    var allowed = await fs.readFile("./ping_allowed", "utf-8").split("\n");
-    if (allowed.contains(msg.member.id)) return;
-    if (msg.mentions.members
-        // If there are staff pings
-        .some(member => {
-            // If the message mentions any members that satisfy the following:
-            return member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1)
-    // Tell them off:
-    })) await msg.channel.send(`Hey ${msg.author.username}! Please don't tag staff members.`)
-}
-
 module.exports = function (client) {
 
     client.on("message", async msg => {
@@ -21,7 +9,7 @@ module.exports = function (client) {
         if (msg.mentions.members.size === 0) return;
 
         var sender_is_staff = msg.member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
-        if (!sender_is_staff) await no_ping_regular_handler(msg)
+        if (!sender_is_staff) await msg.channel.send(`Hey ${msg.author.username}! Please don't tag staff members.`)
         
     })
 }
