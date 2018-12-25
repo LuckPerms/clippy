dc85c40f88ac27f9880695844c5c4e059c26ce85
Jay Williams <course@jaywilliams.me>
2018-12-25 09:20:44 +0000

    Update no_ping.js to actually work

--- modules/no_ping.js
+++ modules/no_ping.js
@@ -9,7 +9,20 @@ module.exports = function (client) {
         if (msg.mentions.members.size === 0) return;
 
         var sender_is_staff = msg.member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
-        if (!sender_is_staff) await msg.channel.send(`Hey ${msg.author.username}! Please don't tag staff members.`)
+        if (!sender_is_staff) {
+            if (msg.mentions.members
+                    // If there are staff pings
+                    .some(member => {
+                        // If the message mentions any members that satisfy the following:
+                        return member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1)
+    
+                    })
+            ) {
+                // Tell them off:
+                await msg.channel.send(`Hey ${msg.author.username}! Please don't tag staff members.`)
+            }
+
+        }
         
     })
 }
