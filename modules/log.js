7a59713e79ab40a40e90ea66ae620857d6a0727e
Jay Williams <codingJWilliams@users.noreply.github.com>
2018-12-27 21:03:43 +0000

    Remove redundant pad function

--- modules/log.js
+++ modules/log.js
@@ -2,12 +2,6 @@ const discord = require("discord.js");
 var dateFormat = require('dateformat');
 const fs = require("fs");
 
-function pad(s, l) {
-  l = l ? l : 2 // set to 2 if undefined
-  s = s.toString() // make sure
-  return (("0".repeat(l)).substring(0, l - s.length) + s)
-}
-
 const getLogFileName = (date) => dateFormat(date, "yyyy-mm-dd")
 const getLogFileTime = (date) => dateFormat(date, "hh-MM-ss TT")
 
