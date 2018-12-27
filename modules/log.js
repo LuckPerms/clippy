21e59d59e001b1d8fae53f146fcb6d140f60b808
Jay Williams <codingJWilliams@users.noreply.github.com>
2018-12-27 20:20:27 +0000

    Pad for neatness :^)

--- modules/log.js
+++ modules/log.js
@@ -1,14 +1,20 @@
 const discord = require("discord.js");
 const fs = require("fs");
 
+function pad(s, l) {
+  l = l ? l : 2 // set to 2 if undefined
+  s = s.toString() // make sure
+  return (("0".repeat(l)).substring(0, l - s.length) + s)
+}
+
 function getLogFileName() {
   var d = new Date();
-  return `${d.getYear() + 1900}-${d.getMonth() + 1}-${d.getDate()}.log`
+  return `${d.getYear() + 1900}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}.log`
 }
 
 function getLogFileTime() {
   var d = new Date();
-  return `${(d.getHours() > 12) ? (d.getHours() - 12) : d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ${(d.getHours() > 12) ? "PM" : "AM"}`
+  return `${(d.getHours() > 12) ? pad(d.getHours() - 12) : pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())} ${(d.getHours() > 12) ? "PM" : "AM"}`
 }
 
 module.exports = (client) => {
