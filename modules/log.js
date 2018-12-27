97b2baac25b28a80033d47f3f0caf38ae2b3313d
Jay Williams <codingJWilliams@users.noreply.github.com>
2018-12-27 20:04:53 +0000

    Should work but untested

--- modules/log.js
+++ modules/log.js
@@ -1,6 +1,25 @@
-
+const discord = require("discord.js");
+const fs = require("fs");
 
 function getLogFileName() {
-  var d = new Date()
+  var d = new Date();
   return `${d.getYear() + 1900}-${d.getMonth() + 1}-{d.getDate()}.log`
+}
+
+function getLogFileTime() {
+  var d = new Date();
+  return `${((d.getHours() + 1) > 12) ? ((d.getHours() + 1) - 12) : (d.getHours() + 1)}:${d.getMinutes()}:${d.getSeconds()} ${((d.getHours() + 1) > 12) ? "PM" : "AM"}`
+}
+
+module.exports = (client) => {
+  client.on("message", (message) => {
+    var log = `${getLogFileTime()} [INFO] [${message.channel.name}] ${message.author.tag}: ${message.content}`
+    fs.appendFile(
+      getLogFileName(), 
+      log,
+      function (err) {
+        if (err) console.log(err);
+      }
+    )
+  })
 }
