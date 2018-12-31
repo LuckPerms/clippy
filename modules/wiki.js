e85992d2bf4be8b9a67db59ec0ba7ee2fd090ecd
Andre_601 <11576465+Andre601@users.noreply.github.com>
2018-12-31 18:49:05 +0100

    Would that work?

--- modules/wiki.js
+++ modules/wiki.js
@@ -4,10 +4,10 @@ module.exports = function (client) {
     client.on("message", async msg => {
         // Ignore DMs and messages that don't start with the prefix
         if (msg.channel.type !== "text") return;
-        if (!msg.content.startsWith("!")) return;
+        if (!msg.content.startsWith("!") || msg.author.bot) return;
         
         // Check if command is in the data file. If so, send it
         var wiki_link = data.wiki_links[msg.content.toLowerCase().substring(1)];
-        if (wiki_link) await msg.channel.send(`:paperclip: <${wiki_link}>`)
+        if (wiki_link) await msg.channel.send(`:bookmark: Wiki: \`` + msg.content.toLowerCase().substring(1) + `\`\n${wiki_link}`)
     })
 }
