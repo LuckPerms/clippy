3526d8c642af6c70994f309f8b65a6072d3c9635
Andre_601 <11576465+Andre601@users.noreply.github.com>
2019-01-02 13:50:42 +0100

    More informative command responses (#3)
    * Make responses more informative
    
    * Would that work?
    
    * Fix up some of the grammar
    
    * Fix possible implications of echoing user input back
    
    Remove non A-Z, a-z, 1-9 characters from the trigger, and only allow one word so no @everyone etc

--- modules/wiki.js
+++ modules/wiki.js
@@ -4,10 +4,13 @@ module.exports = function (client) {
     client.on("message", async msg => {
         // Ignore DMs and messages that don't start with the prefix
         if (msg.channel.type !== "text") return;
-        if (!msg.content.startsWith("!")) return;
+        if (!msg.content.startsWith("!") || msg.author.bot) return;
         
         // Check if command is in the data file. If so, send it
         var wiki_link = data.wiki_links[msg.content.toLowerCase().substring(1)];
-        if (wiki_link) await msg.channel.send(`:paperclip: <${wiki_link}>`)
+        if (wiki_link) {
+            var trigger = msg.content.toLowerCase().substring(1).split(" ")[0].replace(/[^0-9a-z]/gi, '')
+            await msg.channel.send(`:bookmark: Wiki: \`` + trigger + `\`\n${wiki_link}`)
+        }
     })
 }
