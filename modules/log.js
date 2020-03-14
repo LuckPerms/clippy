bc5e24bcce0ca323a4e4eba0ef97b8aee2a4417b
Sam Goodger <sam@goodger.nz>
2020-03-14 15:43:08 +1100

    Reformat code and various other improvements

--- modules/log.js
+++ modules/log.js
@@ -1,20 +1,21 @@
 const dateFormat = require('dateformat');
-const fs = require("fs");
+const fs = require('fs');
 
-const getLogFileName = (date) => dateFormat(date, "yyyy-mm-dd") + ".log";
-const getLogFileTime = (date) => dateFormat(date, "hh-MM-ss TT");
+const getLogFileName = (date) => 'logs/' + dateFormat(date, 'yyyy-mm-dd') + '.log';
+const getLogFileTime = (date) => dateFormat(date, 'hh-MM-ss TT');
 
 module.exports = (client) => {
-    client.on("message", (message) => {
-        const log = `${getLogFileTime()} [${message.channel.name}] ${message.author.tag}: ${message.content}\n`;
-        fs.appendFile(
-            getLogFileName(),
-            log,
-            function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            }
-        );
-    });
+  client.on('message', (message) => {
+    const log = `${getLogFileTime()} [${message.channel.name}] ${message.author.tag}: ${message.content}\n`;
+
+    fs.appendFile(
+        getLogFileName(),
+        log,
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+    );
+  });
 };
