0bd43e4550125eb9afed1652d49ba106fa4365ba
underscore11code <minecrafter11mrt@gmail.com>
2020-04-24 01:12:59 -0700

    Add pastebin parser

---
+++ modules/pastebins.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const RichEmbed = require('discord.js').RichEmbed
+module.exports = function (client) {
+    const pastebins = [{regex: /https?:\/\/hastebin\.com\/(\w+)/g, getLink: 'https://hastebin.com/raw/{code}'},
+        {regex: /https?:\/\/hasteb\.in\/(\w+)/g, getLink: 'https://hasteb.in/raw/{code}'},
+        {regex: /https?:\/\/paste\.helpch\.at\/(\w+)/g, getLink: 'https://paste.helpch.at/raw/{code}'},
+        {regex: /https?:\/\/bytebin\.lucko\.me\/(\w+)/g, getLink: 'https://bytebin.lucko.me/{code}'},]
+    client.on('message', async message => {
+        let getLink;
+        let originalLink;
+        pastebins.forEach(pastebin => {
+            let match = pastebin.regex.exec(message.content);
+            if (match) {
+                getLink = pastebin.getLink.replace('{code}', match[1]);
+                originalLink = match[0];
+            }
+        })
+        if (!getLink) return;
+
+        let embed = new RichEmbed().setTitle(originalLink)
+        axios.get(getLink).then(response => {
+            if (response.data.length >=  2000)
+                embed.setColor('#FF0000').setDescription('Contents exceed 2000 characters')
+            else
+                embed.setColor('#00FF00').setDescription(response.data)
+
+        }).catch(e => {
+            if (e.response)
+                embed.setColor('#FF0000').setDescription(`${e.response.status} ${e.response.statusText}`)
+            else {
+                embed.setColor('#FF0000').setDescription('An internal error occurred.')
+                console.error(e)
+            }
+        }).finally(() => {
+            message.channel.send(embed)
+        })
+    })
+}
