a474e925c3be63541d72b45ed359213edbdd3345
underscore11code <minecrafter11mrt@gmail.com>
2020-05-16 22:21:54 -0700

    fix up code style

--- modules/autoupload.js
+++ modules/autoupload.js
@@ -8,13 +8,17 @@ module.exports = client => {
     client.on('message', async message => {
         if (message.channel.type !== 'text' || message.author.bot) return;
         if (!message.attachments) return;
-        for (let attachment of message.attachments.values()) {
+        for (const attachment of message.attachments.values()) {
             if (!contentTypes.some(type => mimeType.lookup(attachment.url) === type)) continue;
-            let content = await axios.get(attachment.url);
-            let response = await axios.post(`${bytebin}/post`, content.data, {
-                headers: {'Content-Type': 'text/raw'}
-            });
-            await message.channel.send(`Please use ${bytebin} to send files in the future. I have automatically uploaded \`${attachment.filename}\` for you: ${bytebin}/${response.data.key}`);
+            try {
+                let content = await axios.get(attachment.url);
+                let response = await axios.post(`${bytebin}/post`, content.data, {
+                    headers: {'Content-Type': 'text/raw'}
+                });
+                await message.channel.send(`Please use ${bytebin} to send files in the future. I have automatically uploaded \`${attachment.filename}\` for you: ${bytebin}/${response.data.key}`);
+            } catch (e) {
+                await message.channel.send(`Your file could not be automatically uploaded to bytebin. Please use ${bytebin} to share files.`)
+            }
         }
     })
 }
