31310686e21b42906445467f7827f7546bc917f3
Sam Goodger <sam@goodger.nz>
2020-05-21 09:25:03 +1000

    Don't respond to just "!"

--- modules/commands/index.js
+++ modules/commands/index.js
@@ -21,7 +21,7 @@ module.exports = function (client) {
   client.on('message', async message => {
     // Ignore DMs and messages that don't start with the prefix
     if (message.channel.type !== 'text') return;
-    if (!message.content.startsWith('!') || message.author.bot) return;
+    if (!message.content.startsWith('!') || message.author.bot || message.content === '!') return;
 
     // Grab the command
     const trigger = message.content.toLowerCase().substring(1).split(' ')[0].replace(/[^0-9a-z]/gi, '');
