cf552c4e80f2b1899d58d68f71ca5b47c061e5f8
Sam Goodger <sam@goodger.nz>
2020-06-01 03:09:29 +1000

    Improve trigger bugfix

--- modules/commands/index.js
+++ modules/commands/index.js
@@ -21,10 +21,13 @@ module.exports = function (client) {
   client.on('message', async message => {
     // Ignore DMs and messages that don't start with the prefix
     if (message.channel.type !== 'text') return;
-    if (!message.content.startsWith('!') || message.author.bot || message.content === '!') return;
+    if (!message.content.startsWith('!') || message.author.bot) return;
 
     // Grab the command
     const trigger = message.content.toLowerCase().substring(1).split(' ')[0].replace(/[^0-9a-z]/gi, '');
+    
+    // Ignore if trigger is blank
+    if (!trigger) return;
 
     // Initiate the embed
     let embed = new discord.RichEmbed();
