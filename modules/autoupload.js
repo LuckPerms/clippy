31b92f1473834bde1db8890b5ea9c5b676b88f1b
Luck <git@lucko.me>
2020-10-27 23:49:26 +0000

    oops

--- modules/autoupload.js
+++ modules/autoupload.js
@@ -1,4 +1,4 @@
-git const mimeType = require('mime-types');
+const mimeType = require('mime-types');
 const axios = require('axios');
 
 const contentTypes = ['application/json', 'text/plain', 'text/yaml'];
