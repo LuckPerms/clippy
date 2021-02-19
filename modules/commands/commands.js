fc422cf103e95839dd5f3a046cfec8bc786441d6
Sam Goodger <sam@goodger.nz>
2021-02-20 01:01:59 +1100

    stuff

---
+++ modules/commands/commands.js
@@ -0,0 +1,21 @@
+export const commands = {
+  user: {
+    _user: {
+      parent: {
+        add: {
+          _group: {
+
+          }
+        }
+      },
+      permission: {
+        set: {
+          _permission: {
+            _value: {
+            }
+          }
+        }
+      }
+    }
+  }
+}
