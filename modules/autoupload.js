5eb8bef96857ddf1f86e536618450a120bef46e9
Luck <git@lucko.me>
2021-03-27 14:28:39 +0000

    use paste frontend when automatically uploading files

--- modules/autoupload.js
+++ modules/autoupload.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 const contentTypes = ['application/json', 'text/plain', 'text/yaml'];
 const bytebin = 'https://bytebin.lucko.me';
+const paste = 'https://paste.lucko.me';
 
 module.exports = client => {
   client.on('message', async message => {
@@ -11,14 +12,17 @@ module.exports = client => {
     for (const attachment of message.attachments.values()) {
       let contentType = mimeType.lookup(attachment.url);
       if (!contentTypes.some(type => contentType === type)) continue;
+      if (attachment.name.endsWith('.log')) {
+        contentType = 'text/log';
+      }
       try {
         let content = await axios.get(attachment.url);
         let response = await axios.post(`${bytebin}/post`, content.data, {
           headers: {'Content-Type': contentType}
         });
-        await message.channel.send(`Please use ${bytebin} to send files in the future. I have automatically uploaded \`${attachment.name}\` for you: ${bytebin}/${response.data.key}`);
+        await message.channel.send(`Please use ${paste} to send files in the future. I have automatically uploaded \`${attachment.name}\` for you: ${paste}/${response.data.key}`);
       } catch (e) {
-        await message.channel.send(`Your file could not be automatically uploaded to bytebin. Please use ${bytebin} to share files.`)
+        await message.channel.send(`Your file could not be automatically uploaded. Please use ${paste} to share files.`)
       }
     }
   })
