c4a91d2813d65b4b2b42fcf288fae4ce57ce0471
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-04-02 20:10:13 +0800

    Create log folder only if absent. (#53)

--- modules/log.js
+++ modules/log.js
@@ -1,11 +1,17 @@
 const dateFormat = require('dateformat');
 const fs = require('fs');
 
-fs.mkdir('./logs/', (err) => {
-  if (err) throw err;
-});
+const LOG_PATH = './logs/';
 
-const getLogFileName = (date) => 'logs/' + dateFormat(date, 'yyyy-mm-dd') + '.log';
+// Create log folder if absent
+if (!fs.existsSync(LOG_PATH) || !fs.lstatSync(LOG_PATH).isDirectory()) {
+  console.log("Did not find log folder, creating it.");
+  fs.mkdir(LOG_PATH, (err) => {
+    if (err) throw err;
+  });
+}
+
+const getLogFileName = (date) => LOG_PATH + dateFormat(date, 'yyyy-mm-dd') + '.log';
 const getLogFileTime = (date) => dateFormat(date, 'hh-MM-ss TT');
 
 module.exports = (client) => {
