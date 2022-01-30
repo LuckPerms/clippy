88ed459859096a3434f787db73aad18ff3da22fa
Luck <git@lucko.me>
2022-01-30 18:33:15 +0000

    fix autorole

--- modules/auto-role.js
+++ modules/auto-role.js
@@ -1,5 +1,6 @@
 module.exports = (client) => {
   client.on('message', async (message) => {
+    if (!message.member) return;
     const role = process.env.DISCORD_MEMBER_ROLE;
 
     if (!message.member.roles.cache.has(role)) {
