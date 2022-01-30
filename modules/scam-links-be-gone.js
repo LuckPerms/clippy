70d8493ad7d76802caf87d21f5b1e59e10840c55
Luck <git@lucko.me>
2022-01-30 18:27:03 +0000

    Scam links auto-delete

---
+++ modules/scam-links-be-gone.js
@@ -0,0 +1,54 @@
+const legit = [
+  "dis.gd",
+  "discord.co",
+  "discord.com",
+  "discord.design",
+  "discord.dev",
+  "discord.gg",
+  "discord.gift",
+  "discord.gifts",
+  "discord.media",
+  "discord.new",
+  "discord.store",
+  "discord.tools",
+  "discordapp.com",
+  "discordapp.net",
+  "discordmerch.com",
+  "discordpartygames.com",
+  "discord-activities.com",
+  "discordactivities.com",
+  "discordsays.com",
+  "discordstatus.com",
+  "discordapp.io",
+  "discord4j.com",
+  "steamcommunity.com",
+  "steamgames.com",
+  "steampowered.com",
+  "discordcdn.com"
+]
+
+module.exports = (client) => {
+  client.on('message', async (message) => {
+    if (msg.channel.type !== 'text') return;
+    if (msg.member.user.bot) return;
+    if (!msg.content) return;
+
+    let badDomain;
+    const matches = msg.content.matchAll(/https?:\/\/((?:(?:d+[i1l]+[csz]+)|(?:s+[t]+[er]+))[\w.-]+)/gm);
+    for (const match of matches) {
+      const domain = match[1];
+      if (!legit.has(domain)) {
+        badDomain = domain;
+        break;
+      }
+    }
+
+    if (badDomain) {
+      await message.delete();
+      
+      const channel = message.guild.channels.resolve(process.env.DISCORD_PUNISHMENTS_CHANNEL);
+      await channel.send(`Deleted message from ${msg.author.username} (${msg.author.id}) containing likely scam link \`${badDomain}\``);
+    }
+  });
+};
+  
