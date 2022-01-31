d8087e7b47c1336f87272345bca7d0b968ca6205
Luck <git@lucko.me>
2022-01-31 20:52:43 +0000

    improve scam link handling

--- modules/scam-links-be-gone.js
+++ modules/scam-links-be-gone.js
@@ -1,40 +1,47 @@
-const legit = [
-  "dis.gd",
-  "discord.co",
-  "discord.com",
-  "discord.design",
-  "discord.dev",
-  "discord.gg",
-  "discord.gift",
-  "discord.gifts",
-  "discord.media",
-  "discord.new",
-  "discord.store",
-  "discord.tools",
-  "discordapp.com",
-  "discordapp.net",
-  "discordmerch.com",
-  "discordpartygames.com",
-  "discord-activities.com",
-  "discordactivities.com",
-  "discordsays.com",
-  "discordstatus.com",
-  "discordapp.io",
-  "discord4j.com",
-  "steamcommunity.com",
-  "steamgames.com",
-  "steampowered.com",
-  "discordcdn.com"
-]
+const regex = /https?:\/\/((?:(?:d+[i1l]+[csz]+)|(?:s+[t]+[er]+))[\w.-]+)/gm;
 
-module.exports = (client) => {
-  client.on('message', async (msg) => {
+const legit = [
+  'dis.gd',
+  'discord.co',
+  'discord.com',
+  'discord.design',
+  'discord.dev',
+  'discord.gg',
+  'discord.gift',
+  'discord.gifts',
+  'discord.media',
+  'discord.new',
+  'discord.store',
+  'discord.tools',
+  'discordapp.com',
+  'discordapp.net',
+  'discordmerch.com',
+  'discordpartygames.com',
+  'discord-activities.com',
+  'discordactivities.com',
+  'discordsays.com',
+  'discordstatus.com',
+  'discordapp.io',
+  'discord4j.com',
+  'steamcommunity.com',
+  'steamgames.com',
+  'steampowered.com',
+  'discordcdn.com',
+];
+
+let lastTag = {
+  author: '',
+  domain: '',
+};
+
+module.exports = client => {
+  client.on('message', async msg => {
     if (msg.channel.type !== 'text') return;
     if (!msg.content || !msg.member) return;
-    if (msg.member.user.bot) return;
+    if (msg.author.bot) return;
 
     let badDomain;
-    const matches = msg.content.matchAll(/https?:\/\/((?:(?:d+[i1l]+[csz]+)|(?:s+[t]+[er]+))[\w.-]+)/gm);
+    const matches = msg.content.matchAll(regex);
     for (const match of matches) {
       const domain = match[1];
       if (!legit.includes(domain)) {
@@ -46,9 +53,16 @@ module.exports = (client) => {
     if (badDomain) {
       await msg.delete();
 
-      const channel = msg.guild.channels.resolve(process.env.DISCORD_PUNISHMENTS_CHANNEL);
-      await channel.send(`Deleted message from ${msg.author.username} (${msg.author.id}) containing likely scam link \`${badDomain}\``);
+      if (lastTag.author != msg.author.id || lastTag.domain != badDomain) {
+        lastTag = { author: msg.author.id, domain: badDomain };
+
+        const channel = msg.guild.channels.resolve(
+          process.env.DISCORD_PUNISHMENTS_CHANNEL
+        );
+        await channel.send(
+          `Deleted message(s) from ${msg.author} (${msg.author.id}) containing likely scam link \`${badDomain}\``
+        );
+      }
     }
   });
 };
-  
